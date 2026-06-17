b0f4644afe3fe213b98ba1f85229565a36992a20
root <root@localhost.localdomain>
2026-06-17 04:56:10 +0000

    Fix build deps: remove unavailable artifacts, fix MMKV coordinate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("androidx.profileinstaller:profileinstaller:1.4.0")
     implementation("androidx.emoji2:emoji2:1.5.0")
     implementation("androidx.window:window:1.3.0")
-    implementation("androidx.window.extensions:extensions:1.0.0-alpha01")
+    // implementation("androidx.window.extensions:extensions:1.0.0-alpha01") // not available in repos
 
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.guava:guava:33.3.1-android")
@@ -89,9 +89,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-io:0.2.0")
+    // implementation("org.jetbrains.kotlinx:kotlinx-io:0.2.0") // not available in repos
 
-    implementation("com.tencent:mmkv:mmkv:1.3.9")
+    implementation("com.tencent:mmkv:1.3.9")
     implementation("com.sun.mail:android-mail:1.6.7")
     implementation("com.sun.mail:android-activation:1.6.7")
 
@@ -100,7 +100,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-    implementation("com.termux:termux-shared:0.118.0")
+    // implementation("com.termux:termux-shared:0.118.0") // not available on Maven; using local Termux source
 
     // Compose
     implementation(platform("androidx.compose:compose-bom:2024.11.00"))
